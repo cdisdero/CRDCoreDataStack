552878bda31023ce2a3f5431215f5a1ca289df50
Christopher Disdero <info@code.chrisdisdero.com>
2017-10-02 19:20:27 -0700

    Initial Commit

---
+++ CRDCoreDataStack/CRDCoreDataStack.h
@@ -0,0 +1,19 @@
+//
+//  CRDCoreDataStack.h
+//  CRDCoreDataStack
+//
+//  Created by Christopher Disdero on 10/2/17.
+//  Copyright © 2017 Christopher Disdero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CRDCoreDataStack.
+FOUNDATION_EXPORT double CRDCoreDataStackVersionNumber;
+
+//! Project version string for CRDCoreDataStack.
+FOUNDATION_EXPORT const unsigned char CRDCoreDataStackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRDCoreDataStack/PublicHeader.h>
+
+
